73c9ce7dadcb406f93fc3c227fbdc6dca231345c
zkenn <zkenn@bu.edu>
2021-03-28 18:53:42 -0400

    Update admin.py

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import County
+from .models import County, State
 
 class CountyAdmin(admin.ModelAdmin):
     list_display = ('name', 'state_name', 'stats')
@@ -7,3 +7,4 @@ class CountyAdmin(admin.ModelAdmin):
 # Register your models here.
 
 admin.site.register(County, CountyAdmin)
+admin.site.register(State)
