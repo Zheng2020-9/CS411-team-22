04c861d6afb5d98165e8b5fb0dcbb6d7c676923a
Stephen / Sharp <Sharpcastle33@users.noreply.github.com>
2021-03-28 18:18:08 -0400

    added State view

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import CountySerializer
+from .serializers import StateSerializer
 from .models import County
+from .models import State
 
 # Create your views here.
 
 class CountyView(viewsets.ModelViewSet):
     serializer_class = CountySerializer
     queryset = County.objects.all()
+    
+class StateView(viewsets.ViewSet):
+
+    lookup_field = 'name'
+    
+    def list(self, request):
+        queryset = State.objects.all()
+        serializer = StateSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, name=None):
+        queryset = State.objects.all()
+        state = get_object_or_404(queryset, name=name)
+        serializer = StateSerializer(state)
+        return Response(serializer.data)   
