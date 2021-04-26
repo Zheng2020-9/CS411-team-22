2315a81b916036143cf8dcb29a8d921e82058ff1
vivian271 <79332719+vivian271@users.noreply.github.com>
2021-04-25 20:03:41 -0400

    now user can sign in and still needed to display info

--- prototype/frontend/src/App.js
+++ prototype/frontend/src/App.js
@@ -1,38 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 import InfoForm from './InfoForm.js';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from "axios";
-import GoogleBtn from './GoogleBtn';
 
 
 
-function App() {
-	
-	
 
+class App extends React.Component {
 
+  insertGapiScript() {
+    const script = document.createElement('script')
+    script.src = 'https://apis.google.com/js/api.js'
+    script.onload = () => {
+      this.initializeGoogleSignIn()
+    }
+    document.body.appendChild(script)
+  }
 
-  
+  initializeGoogleSignIn() {
+    window.gapi.load('auth2', () => {
+      window.gapi.auth2.init({
+        client_id: '277403998690-n37ruvh63adulkb70tr21of851iducdc.apps.googleusercontent.com'
+      })
+      console.log('Api inited')
 
-  return (
-	<div className = 'container'>
-    <div className='row center'>
-	  <h1>Covid Dashboard App</h1>
-		<div className='row center'>
+      window.gapi.load('signin2', () => {
+        const params = {
+          onsuccess: () => {
+            console.log('User has finished signing in!')
+          }
+        }
+        window.gapi.signin2.render('loginButton', params)
+      })
+    })
+  }
+
+  componentDidMount() {
+    console.log('Loading')
+
+    this.insertGapiScript();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Covid Dashboard</h1>
+        <a id="loginButton">Sign in with Google</a>
+        		<div className='row center'>
 		<InfoForm />
-
-		<GoogleBtn/>
-
 		</div>
-    </div>
-	</div>
-	
-
-  );
+      </div>
+    );
+  }
 }
-
-  ReactDOM.render(<App />, document.getElementById('root'));
-
 
 export default App;
