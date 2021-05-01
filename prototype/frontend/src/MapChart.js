df19b854d554ed7dc863f5be31fe373ea4627165
vivian271 <79332719+vivian271@users.noreply.github.com>
2021-05-01 00:54:52 -0400

    Created Hover

--- prototype/frontend/src/MapChart.js
+++ prototype/frontend/src/MapChart.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { ZoomableGroup, ComposableMap, Geographies, Geography } from "react-simple-maps";
 import { scaleQuantile } from "d3-scale";
 import { json } from "d3-fetch";
 import { csv } from "d3-fetch";
@@ -9,104 +9,128 @@ import axios from "axios";
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json";
 
-const MapChart = () => {
-  const [data, setData] = useState([]);
+const rounded = num => {
+	if (num > 1000000000) {
+	  return Math.round(num / 100000000) / 10 + "Bn";
+	} else if (num > 1000000) {
+	  return Math.round(num / 100000) / 10 + "M";
+	} else {
+	  return Math.round(num / 100) / 10 + "K";
+	}
+  };
 
-  useEffect(() => {
-	//CURRENTLY IMPORTING DIRECTLY, UPDATE TO USE DB
-	csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv").then(counties => {
-		console.log(counties)
-		for(var i in counties){
-			counties[i].id = counties[i].fips
-			counties[i].name = counties[i].county
-		}
-      setData(counties);
-    });
-	
-	const source = axios.CancelToken.source();
-	
-	//WE WILL USE THIS LATER WHEN WE IMPORT FROM DB
-	
-	//asyn and await allow for standard cleanup (react gets fussy when you do async stuff on components)
-	const fetchData = async () => {
-		try {
-			const response = await axios.get("/api/Counties", {
-				cancelToken: source.token
-			}).then((res)=>{
-				var reply = res.data;
-				var values = Object.values(reply)
+const MapChart = ({ setTooltipContent }) => {
+	const [data, setData] = useState([]);
 
-				for(var i in values){
-					values[i].name = values[i].county_name + " County";
-					values[i].id = i;
-				}
-				//console.log(values)
-				//setData(values)
-			})
-		}catch(error) {
-			if(axios.isCancel(error)){
-			
-			}else{
-				throw error
+	useEffect(() => {
+		//CURRENTLY IMPORTING DIRECTLY, UPDATE TO USE DB
+		csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv").then(counties => {
+			console.log(counties)
+			for (var i in counties) {
+				counties[i].id = counties[i].fips
+				counties[i].name = counties[i].county
 			}
-		}
-	};
-	
-	fetchData();
-	
-	return () => {
-		source.cancel();
-	};
-	//axios.get("/api/Counties/").then((res) => {
-	//	var reply = res.data;
-	//	var values = Object.values(reply)
-	//	var counties = []
-		
-	//	for(var i in reply)
-	//		counties.push(values[i]);
+			setData(counties);
+		});
+
+		const source = axios.CancelToken.source();
+
+		//WE WILL USE THIS LATER WHEN WE IMPORT FROM DB
+
+		//asyn and await allow for standard cleanup (react gets fussy when you do async stuff on components)
+		const fetchData = async () => {
+			try {
+				const response = await axios.get("/api/Counties", {
+					cancelToken: source.token
+				}).then((res) => {
+					var reply = res.data;
+					var values = Object.values(reply)
+
+					for (var i in values) {
+						values[i].name = values[i].county_name + " County";
+						values[i].id = i;
+					}
+					//console.log(values)
+					//setData(values)
+				})
+			} catch (error) {
+				if (axios.isCancel(error)) {
+
+				} else {
+					throw error
+				}
+			}
+		};
+
+		fetchData();
+
+		return () => {
+			source.cancel();
+		};
+		//axios.get("/api/Counties/").then((res) => {
+		//	var reply = res.data;
+		//	var values = Object.values(reply)
+		//	var counties = []
+
+		//	for(var i in reply)
+		//		counties.push(values[i]);
 		//console.log(reply)
-	//	console.log(counties)
-	//	setData(counties)
+		//	console.log(counties)
+		//	setData(counties)
 		//json(reply).then(counties => {
 		//	console.log(counties)
 		//	setData(counties);//
 		//});
 
-	//}).catch();
-  }, []);
+		//}).catch();
+	}, []);
 
-  const colorScale = scaleQuantile()
-    .domain(data.map(d => d.cases))
-    .range([
-      "#ffedea",
-      "#ffcec5",
-      "#ffad9f",
-      "#ff8a75",
-      "#ff5533",
-      "#e2492d",
-      "#be3d26",
-      "#9a311f",
-      "#782618"
-    ]);
+	const colorScale = scaleQuantile()
+		.domain(data.map(d => d.cases))
+		.range([
+			"#ffedea",
+			"#ffcec5",
+			"#ffad9f",
+			"#ff8a75",
+			"#ff5533",
+			"#e2492d",
+			"#be3d26",
+			"#9a311f",
+			"#782618"
+		]);
 
-  return (
-    <ComposableMap projection="geoAlbersUsa">
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map(geo => {
-            const cur = data.find(s => s.id === geo.id);
-            return (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                fill={cur ? colorScale(cur.cases) : "#EEE"}
-              />
-            );
-          })
-        }
-      </Geographies>
-    </ComposableMap>
-  );
+	return (
+		<ComposableMap projection="geoAlbersUsa">
+			<Geographies geography={geoUrl}>
+				{({ geographies }) =>
+					geographies.map(geo => {
+						const cur = data.find(s => s.id === geo.id);
+						return (
+							<Geography
+								key={geo.rsmKey}
+								geography={geo}
+								fill={cur ? colorScale(cur.cases) : "#EEE"}
+								//tooltip hover functions
+								onMouseEnter={() => {
+									const { NAME, Cov_EST } = geo.properties;
+									setTooltipContent(`${NAME} — ${rounded(Cov_EST)}`);
+								  }}
+								onMouseLeave={() => {
+									setTooltipContent("");
+								  }}
+								  style={{
+									hover: {
+									  fill: "#0000FF",
+									  outline: "none"
+									}
+								  }}
+							/>
+						);
+					})
+				}
+			</Geographies>
+		</ComposableMap>
+	);
 };
 
 export default MapChart;
