17e4cc0418c9d655a4c159ead46879988e4f8582
vivian271 <79332719+vivian271@users.noreply.github.com>
2021-05-03 03:47:01 -0400

    add bookmark

--- prototype/frontend/src/App.js
+++ prototype/frontend/src/App.js
@@ -7,45 +7,30 @@ import ReactDOM from 'react-dom';
 import axios from "axios";
 import ReactTooltip from "react-tooltip";
 import React, { useState, useEffect } from "react";
-import BookmarkSidebar from './BookmarkSidebar';
 
 
-function onClick(e, item) {
-  console.log(JSON.stringify(item, null, 2));
+function addbookmark() {
+	var county = localStorage.getItem('county');
+	console.log("Adding bookmark");
+	const params = {token: localStorage.getItem('token'), command: "addBM", BM: county};
+		console.log(JSON.stringify(params));
+	try{
+			 axios.post('/useroperate/', params);
+		//	console.log(response.data);
+	}catch(err){
+
+	}
 }
-
-	const items = [
-  { name: 'home', label: 'Home', onClick },
-  { name: 'home', label: 'About' },
-  {
-    name: 'bookmarks',
-    label: 'Bookmarks',
-    items: [
-      { name: 'a', label: 'County A', onClick },
-      { name: 'b', label: 'County B', onClick },
-    ],
-  },
-]
-
-
 
 
 
 function App() {
-
-
-
-
-
 	const [data, setData] = useState([]);
-	
-
-
 
 	return (
 		<div className='container'>
 			<div className='row center'>
-				<BookmarkSidebar items={items}/>
+
 				<h1>Covid Dashboard App</h1>
 				<div className='row center'>
 					<InfoForm />
@@ -56,8 +41,9 @@ function App() {
 					
 					<MapChart setTooltipContent={setData} />
      				<ReactTooltip>{data}</ReactTooltip>
-					
-
+				
+					 <button onClick={() => addbookmark()}>Add Bookmark Button
+     				 </button>
 					
 					
 				</div>
